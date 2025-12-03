7e7b08bc89c4a7db7fb131b802c14cb96a133bb9
Rosen Ivanov <rrirrirri08@gmail.com>
2025-12-03 21:36:08 +0200

    feat: did some things

--- week09/index.php
+++ week09/index.php
@@ -7,8 +7,20 @@
     try {
         $connection = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
+//        echo "Connected successfully";
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
+    }
+
+    $stmt = $connection->prepare("SELECT * FROM product");
+    $stmt->execute();
+    $products = $stmt->fetch();
+
+    foreach ($products as $product) {
+        echo "<section class='sell-items'>";
+        echo "<section class='item-for-sale'>";
+        
+        echo "</section>";
+        echo "</section>";
     }
 ?>
