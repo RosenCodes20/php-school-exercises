f074d308e420510af42b4cd717bf3bdc21ca4252
Rosen Ivanov <rrirrirri08@gmail.com>
2025-12-03 21:48:43 +0200

    feat: fetched all products

--- week09/index.php
+++ week09/index.php
@@ -12,15 +12,20 @@
         echo "Connection failed: " . $e->getMessage();
     }
 
-    $stmt = $connection->prepare("SELECT * FROM product");
+    $stmt = $connection->prepare("SELECT productType, 
+       productImageLink, productsCount, productProducer FROM product");
     $stmt->execute();
     $products = $stmt->fetch();
 
     foreach ($products as $product) {
+        echo($product);
         echo "<section class='sell-items'>";
-        echo "<section class='item-for-sale'>";
-        
-        echo "</section>";
+            echo "<section class='item-for-sale'>";
+                echo "<img src=''>";
+                echo "<div class='text-items'>";
+
+                echo "</div>";
+            echo "</section>";
         echo "</section>";
     }
 ?>
