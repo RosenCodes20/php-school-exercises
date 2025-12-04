e0eec888ec8509d7b4578495af4c4ed4fd8d85b0
Rosen Ivanov <rrirrirri08@gmail.com>
2025-12-04 09:02:44 +0200

    feat: implemented the new login file

--- week09/adminLogin.php
+++ week09/adminLogin.php
@@ -1,1 +1,22 @@
 <?php
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="login.css">
+    </head>
+
+    <body>
+    <article class="login">
+        <form method="post">
+            <h2>Вход</h2>
+            <label for="username">Имейл:</label>
+            <input type="email" name="email" id="username" class="email" placeholder="Въведи твоя имейл......">
+            <label for="pass">Парола:</label>
+            <input type="password" name="password" id="pass" class="password" placeholder="Въведи твоята парола.......">
+
+            <button class="login-btn">Вход</button>
+        </form>
+    </article>
+    </body>
+</html>
