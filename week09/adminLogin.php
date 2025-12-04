16de293d8c9802439f86ca0f4de2dc3242b569b6
Rosen Ivanov <rrirrirri08@gmail.com>
2025-12-04 09:07:26 +0200

    feat: got the email and pass

--- week09/adminLogin.php
+++ week09/adminLogin.php
@@ -1,4 +1,29 @@
 <?php
+    $servername = "127.0.0.1";
+    $username = "myuser";
+    $password = "mypassword";
+    $database = "products";
+
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //        echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+
+    if (isset($_POST['submit'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $stmt = $connection->prepare("SELECT * FROM admins WHERE email = ? AND password = ?");
+        $stmt->execute([$email, $password]);
+
+        $user = $stmt->fetch();
+
+        print_r($user);
+    }
+
 ?>
 
 <html>
