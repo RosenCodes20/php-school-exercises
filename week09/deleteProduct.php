342d48a0326cdbf4290d4e97a0ece133062b5b0c
Rosen Ivanov <rrirrirri08@gmail.com>
2025-12-04 10:10:15 +0200

    feat: added connection and if

--- week09/deleteProduct.php
+++ week09/deleteProduct.php
@@ -1,4 +1,20 @@
 <?php
+    $servername = "127.0.0.1";
+    $username = "myuser";
+    $password = "mypassword";
+    $database = "products";
+
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //        echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+
+    if (isset($_POST['submit'])) {
+
+    }
 ?>
 
 <html>
