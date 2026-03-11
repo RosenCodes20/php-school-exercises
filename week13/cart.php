79e4e8998638af2498f30fa72364b1b16ce8e55c
Rosen Ivanov <rrirrirri08@gmail.com>
2026-03-11 18:23:42 +0200

    feat: did all the exercise wotking really good, used inner join also happy about that

--- week13/cart.php
+++ week13/cart.php
@@ -1,30 +1,3 @@
-<html>
-<head>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
-    <a class="cart-btn" href="products.php">Продукти</a>
-    <div class="cart">
-        <div class="item">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCKYey1jZCIVi3Y_BVPBt6ddOE44oECyb35g&s">
-            <h4>Портокал</h4>
-            <p>Това е просто портокал</p>
-            <a><i class="fa-solid fa-x"></i></a>
-        </div>
-
-        <div class="item">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCKYey1jZCIVi3Y_BVPBt6ddOE44oECyb35g&s">
-            <h4>Портокал</h4>
-            <p>Това е просто портокал</p>
-            <a><i class="fa-solid fa-x"></i></a>
-        </div>
-
-        <a class="finish_order">Завърши поръчката</a>
-    </div>
-</body>
-</html>
-
 <?php
     $servername = "127.0.0.1";
     $username = "myuser";
@@ -37,4 +10,42 @@
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
+
+    if (isset($_POST['delete'])) {
+        $deleteSql = "DELETE FROM cart WHERE id = ?";
+        $stmt = $connection->prepare($deleteSql);
+        $stmt->execute([$_POST["foundProductId"]]);
+    }
+
+    $sql = "SELECT c.id,  p.image, p.productName, p.productDescription
+            FROM products p
+            INNER JOIN cart c
+            ON p.id = c.productId";
+
+    $foundCartItems = $connection->query($sql)->fetchAll();
 ?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <a class="cart-btn" href="products.php">Продукти</a>
+    <div class="cart">
+        <?php foreach ($foundCartItems as $item):?>
+        <div class="item">
+            <img src=<?php echo $item['image'];?>>
+            <h4><?php echo $item['productName'];?></h4>
+            <p><?php echo $item['productDescription'];?></p>
+            <form method="post">
+                <input type="hidden" name="foundProductId" value="<?php echo $item['id'];?>">
+                <button name="delete" type="submit"><i class="fa-solid fa-x"></i></button>
+            </form>
+            <a></a>
+        </div>
+        <?php endforeach; ?>
+        <a class="finish_order">Завърши поръчката</a>
+    </div>
+</body>
+</html>
